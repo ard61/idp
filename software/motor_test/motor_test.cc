2ebeaf6427ca3cd564ae5b8330506e821bc92963
Antoine Dupuis <ard61@cam.ac.uk>
2016-02-19 13:29:03 +0000

    New motor_test program.

---
+++ software/motor_test/motor_test.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+
+#include <robot_link.h>
+#include <robot_instr.h>
+
+#include <delay.h>
+
+int main(int argc, char* argv[]) {
+  robot_link rlink;
+  int rc;
+  const int DELAY_MSECS = 1000;
+  const command_instruction instruction_array[8] = {MOTOR_1_GO, MOTOR_2_GO, MOTOR_3_GO, MOTOR_4_GO};
+
+  #ifdef __arm__
+  rc = rlink.initialise();
+  #else
+  const int ROBOT_NUM = 9;
+  rc = rlink.initialise(ROBOT_NUM);
+  #endif
+
+  if (!rc) {
+    std::cout << "Cannot initialise link." << std::endl;
+    return -1;
+  }
+  
+  while (!rlink.fatal_err())
+  {
+    for (int i = 0; i < 4; i++) {
+
+    std::cout << "Testing motor " << i+1 << ". " << std::endl;
+      rlink.command(instruction_array[i], 127);
+
+      for (int j = 0; j < 4; j++) {
+        if (j != i) rlink.command(instruction_array[j], 0);
+      }
+
+    delay(DELAY_MSECS);
+    }
+  }
+
+  rlink.print_errs();
+
+  return 0;  
+}
