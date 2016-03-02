6c06dc88d2830409db29f4b808e4dca42a7e5a9e
Antoine Dupuis <ard61@cam.ac.uk>
2016-03-02 23:59:32 +0000

    Corrections to the playing area map algorithm.

--- software/map/map.cc
+++ software/map/map.cc
@@ -1,16 +1,19 @@
 #include <fstream>
-#include <iostream>
 #include <cstring>
 #include <cstdlib>
 #include <string>
+#include <iostream>
 
-#include "map.h"
+#include "logging.h"
+#include "robot.h"
 
-idp::Map::Map(){
-  std::strcpy("playing_area.map", map_filename);
+stopwatch idp::logging::logging_stopwatch;
+
+idp::Map::Map() {
+
 }
 
-int idp::Map::populate() {
+int idp::Map::populate(const char* map_file) {
   /*  Function that parses the .map file containing nicely human-readable
       information about point and line positioning on the playing area.
       This function contains absolutely horrible code with c-string handling.
@@ -19,26 +22,29 @@ int idp::Map::populate() {
       at this horrible code anymore.  It hurts my brain.  
   */
 
-  std::ifstream map_file;
-  map_file.open(map_filename);
+  std::ifstream ifs;
+  ifs.open(map_file);
 
   // Variables used inside loop
+  int line_number = 0;
   char cur_line[16];
-  int line_num = 1;
   bool are_dealing_with_points = true;
   Point current_point;
   Line current_line;
 
-  map_file.getline(cur_line, 16);
-  if (std::string(cur_line) != "POINTS:\n") {
+  line_number++;
+  ifs.getline(cur_line, 16);
+  if (strncmp(cur_line, "POINTS:", 7) != 0) {
     // Sanity check here
-    std::cout << "Invalid map file specified.";
+    IDP_ERR << "Error parsing playing area map file on line 1." << std::endl;
     return -1;
   }
 
-  while (map_file.good()) {
-    map_file.getline(cur_line, 16);
-    if (std::string(cur_line) == "LINES:\n") are_dealing_with_points = false;
+  while (ifs.good()) {
+    line_number++;
+    ifs.getline(cur_line, 16);
+
+    if (strncmp(cur_line, "LINES:", 6) == 0) are_dealing_with_points = false;
 
     else if (are_dealing_with_points) {
       // Using the *HORRIBLE* std::strtok function, which is probably
@@ -48,76 +54,150 @@ int idp::Map::populate() {
       // occur.
 
       char* strtok_return;
+      bool error = false;
 
       strtok_return = std::strtok(cur_line, " \n");
-      if (strtok_return != NULL) strcpy(strtok_return, current_point.name);
-      else return -1;
+      if (strtok_return != NULL) {
+        strncpy(current_point.name, strtok_return, 2);
+        current_point.name[2] = '\0';
+      }
+      else { 
+        error = true;
+      }
 
       strtok_return = std::strtok(NULL, " \n");
-      if (strtok_return != NULL) current_point.coord_x = std::atof(strtok_return);
-      else return -1;
-      
+      if (strtok_return != NULL) {
+        current_point.position.x = std::atof(strtok_return);
+      }
+      else { 
+        error = true;
+      }
+
       strtok_return = std::strtok(NULL, " \n");
-      if (strtok_return != NULL) current_point.coord_y = std::atof(strtok_return);
-      else return -1;
+      if (strtok_return != NULL) {
+        current_point.position.y = std::atof(strtok_return);
+      }
+      else { 
+        error = true;
+      }
+
+      strtok_return = std::strtok(NULL, " \n");
+      if (strtok_return != NULL && std::strncmp(strtok_return, "NI", 2) == 0) {
+        current_point.is_intersection = false;
+      }
+      else if (strtok_return == NULL) {
+        current_point.is_intersection = true;
+      }
+      else { 
+        error = true;
+      }
+
+      if (error) {
+        IDP_ERR << "Error parsing playing area map file on line " << line_number << "." << std::endl;
+        return -1;
+      }
 
       // Append to member variable
-      _points.push_back(current_point);
+      points.push_back(current_point);
     }
 
     else { 
       // We're dealing with lines
       
       char* strtok_return;
+      bool error = false;
 
       strtok_return = std::strtok(cur_line, " \n");
-      if (strtok_return != NULL) strcpy(strtok_return, current_line.point1);
-      else return -1;
+      if (strtok_return != NULL) {
+        strncpy(current_line.point1, strtok_return, 2);
+        current_line.point1[2] = '\0';
+      }
+      else error = true;
 
       strtok_return = std::strtok(NULL, " \n");
-      if (strtok_return != NULL) strcpy(strtok_return, current_line.point1);
-      else return -1;
+      if (strtok_return != NULL) {
+        strncpy(current_line.point2, strtok_return, 2);
+        current_line.point2[2] = '\0';
+      }
+      else error = true;
 
       strtok_return = std::strtok(NULL, " \n");
       if (strtok_return != NULL) {
         switch (*strtok_return) {
         case 'S':
           current_line.is_straight = true;
+          break;
         case 'C':
           current_line.is_straight = false;
+          break;
         }
       }
-      else return -1;
+      else error = true;
 
       strtok_return = std::strtok(NULL, " \n");
       if (strtok_return != NULL) {
         switch (*strtok_return) {
         case 'V':
           current_line.orientation = 0;
+          break;
         case 'H':
           current_line.orientation = 1;
+          break;
         case 'P':
           current_line.orientation = 2;
+          break;
         case 'N':
           current_line.orientation = 3;
+          break;
         }
       }
-      else return -1;
+      else error = true;
+
+      if (error) {
+        IDP_ERR << "Error parsing playing area map file on line " << line_number << "." << std::endl;
+        return -1;
+      }
 
       // Append to member variable
-      _lines.push_back(current_line);
+      lines.push_back(current_line);
     }
   }
-  map_file.close();
+  ifs.close();
 
-  std::cout << "Read " << _points.size() << " points and "
-            << _lines.size() << " lines from file." << std::endl;
+  IDP_INFO << "Read " << points.size() << " points and "
+            << lines.size() << " lines from file." << std::endl;
  
   // WHEW!
   return 0;
 }
 
+double idp::Map::distance_from_intersection(idp::Vector2d position) const {
+  double final_distance = 10000; // Dummy large value to start with.  
+
+  for (int i = 0; i < points.size(); i++) {
+    double distance = (position - points[i].position).abs();
+    if (points[i].is_intersection && distance < final_distance) {
+      final_distance = distance;
+    }
+  }
+
+  return final_distance;
+}
+
+
 int main(int argc, char* argv[]) {
-  // This function exists only to make stuff compile.
+  idp::logging::log_init();
+  idp::Map map;
+  map.populate("playing_area.map");
+
+  // Display map contents
+
+  for (int i = 0; i < map.points.size(); i++) {
+    std::cout << "Point: " << map.points[i].name << ", x-coord: " << map.points[i].position.x << ", y-coord: " << map.points[i].position.y << ", is intersection: " << map.points[i].is_intersection << std::endl;
+  }
+
+  for (int i = 0; i < map.lines.size(); i++) {
+    std::cout << "point1 is " << map.lines[i].point1 << " , point2 is " << map.lines[i].point2 << ", is straight: " << map.lines[i].is_straight << ", orientation: " << map.lines[i].orientation << std::endl;
+  }
   return 0;
 }
--- software/robot/map.cc
+++ software/robot/map.cc
@@ -32,16 +32,17 @@ int idp::Map::populate(const char* map_file) {
 
   line_number++;
   ifs.getline(cur_line, 16);
-  if (!strcmp(cur_line, "POINTS:")) {
+  if (strncmp(cur_line, "POINTS:", 7) != 0) {
     // Sanity check here
-    IDP_ERR << "Error parsing playing area map file on line 1 ." << std::endl;
+    IDP_ERR << "Error parsing playing area map file on line 1." << std::endl;
     return -1;
   }
 
   while (ifs.good()) {
     line_number++;
     ifs.getline(cur_line, 16);
-    if (!strcmp(cur_line, "LINES:")) are_dealing_with_points = false;
+
+    if (strncmp(cur_line, "LINES:", 6) == 0) are_dealing_with_points = false;
 
     else if (are_dealing_with_points) {
       // Using the *HORRIBLE* std::strtok function, which is probably
@@ -54,29 +55,48 @@ int idp::Map::populate(const char* map_file) {
       bool error = false;
 
       strtok_return = std::strtok(cur_line, " \n");
-      if (strtok_return != NULL) strcpy(strtok_return, current_point.name);
-      else error = true;
+      if (strtok_return != NULL) {
+        strncpy(current_point.name, strtok_return, 2);
+        current_point.name[2] = '\0';
+      }
+      else { 
+        error = true;
+      }
 
       strtok_return = std::strtok(NULL, " \n");
-      if (strtok_return != NULL) current_point.position.x = std::atof(strtok_return);
-      else error = true;
-      
-      strtok_return = std::strtok(NULL, " \n");
-      if (strtok_return != NULL) current_point.position.y = std::atof(strtok_return);
-      else error = true;
+      if (strtok_return != NULL) {
+        current_point.position.x = std::atof(strtok_return);
+      }
+      else { 
+        error = true;
+      }
 
       strtok_return = std::strtok(NULL, " \n");
-      if (strtok_return != NULL && std::strcmp(strtok_return, "NI")) current_point.is_intersection = false;
-      else if (strtok_return == NULL) current_point.is_intersection = true;
-      else error = true;
+      if (strtok_return != NULL) {
+        current_point.position.y = std::atof(strtok_return);
+      }
+      else { 
+        error = true;
+      }
+
+      strtok_return = std::strtok(NULL, " \n");
+      if (strtok_return != NULL && std::strncmp(strtok_return, "NI", 2) == 0) {
+        current_point.is_intersection = false;
+      }
+      else if (strtok_return == NULL) {
+        current_point.is_intersection = true;
+      }
+      else { 
+        error = true;
+      }
 
       if (error) {
-        IDP_ERR << "Error parsing playing area map file on line " << line_number << " ." << std::endl;
+        IDP_ERR << "Error parsing playing area map file on line " << line_number << "." << std::endl;
         return -1;
       }
 
       // Append to member variable
-      _points.push_back(current_point);
+      points.push_back(current_point);
     }
 
     else { 
@@ -86,11 +106,17 @@ int idp::Map::populate(const char* map_file) {
       bool error = false;
 
       strtok_return = std::strtok(cur_line, " \n");
-      if (strtok_return != NULL) strcpy(strtok_return, current_line.point1);
+      if (strtok_return != NULL) {
+        strncpy(current_line.point1, strtok_return, 2);
+        current_line.point1[2] = '\0';
+      }
       else error = true;
 
       strtok_return = std::strtok(NULL, " \n");
-      if (strtok_return != NULL) strcpy(strtok_return, current_line.point1);
+      if (strtok_return != NULL) {
+        strncpy(current_line.point2, strtok_return, 2);
+        current_line.point2[2] = '\0';
+      }
       else error = true;
 
       strtok_return = std::strtok(NULL, " \n");
@@ -98,8 +124,10 @@ int idp::Map::populate(const char* map_file) {
         switch (*strtok_return) {
         case 'S':
           current_line.is_straight = true;
+          break;
         case 'C':
           current_line.is_straight = false;
+          break;
         }
       }
       else error = true;
@@ -109,29 +137,33 @@ int idp::Map::populate(const char* map_file) {
         switch (*strtok_return) {
         case 'V':
           current_line.orientation = 0;
+          break;
         case 'H':
           current_line.orientation = 1;
+          break;
         case 'P':
           current_line.orientation = 2;
+          break;
         case 'N':
           current_line.orientation = 3;
+          break;
         }
       }
       else error = true;
 
       if (error) {
-        IDP_ERR << "Error parsing playing area map file on line " << line_number << " ." << std::endl;
+        IDP_ERR << "Error parsing playing area map file on line " << line_number << "." << std::endl;
         return -1;
       }
 
       // Append to member variable
-      _lines.push_back(current_line);
+      lines.push_back(current_line);
     }
   }
   ifs.close();
 
-  IDP_INFO << "Read " << _points.size() << " points and "
-            << _lines.size() << " lines from file." << std::endl;
+  IDP_INFO << "Read " << points.size() << " points and "
+            << lines.size() << " lines from file." << std::endl;
  
   // WHEW!
   return 0;
@@ -140,9 +172,9 @@ int idp::Map::populate(const char* map_file) {
 double idp::Map::distance_from_intersection(idp::Vector2d position) const {
   double final_distance = 10000; // Dummy large value to start with.  
 
-  for (int i = 0; i < _points.size(); i++) {
-    double distance = (position - _points[i].position).abs();
-    if (_points[i].is_intersection && distance < final_distance) {
+  for (int i = 0; i < points.size(); i++) {
+    double distance = (position - points[i].position).abs();
+    if (points[i].is_intersection && distance < final_distance) {
       final_distance = distance;
     }
   }
